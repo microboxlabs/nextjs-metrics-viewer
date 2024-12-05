b6c0e42261e79b638e55703a11818ac75b685759
Fabián Morag <fabianmorag0405@gmail.com>
2024-12-05 20:39:23 -0300

    swaping bar chart to line chart

--- nextjs-metrics-viewer/components/MetricsSection.tsx
+++ nextjs-metrics-viewer/components/MetricsSection.tsx
@@ -6,116 +6,45 @@ import UploadDataModal from "@/components/UploadDataModal";
 
 export default function MetricsSection() {
   const [options, setOptions] = useState({
-    colors: ["#84b6f4", "#fdfd96", "#77dd77", "#ff6961", "#fdcae1"],
-    series: [
-      {
-        name: "Organic",
-        data: [
-          { x: "Mon", y: 231 },
-          { x: "Tue", y: 122 },
-          { x: "Wed", y: 63 },
-          { x: "Thu", y: 421 },
-          { x: "Fri", y: 122 },
-          { x: "Sat", y: 323 },
-          { x: "Sun", y: 111 },
-        ],
-      },
-      {
-        name: "Social media",
-        data: [
-          { x: "Mon", y: 232 },
-          { x: "Tue", y: 113 },
-          { x: "Wed", y: 341 },
-          { x: "Thu", y: 224 },
-          { x: "Fri", y: 522 },
-          { x: "Sat", y: 411 },
-          { x: "Sun", y: 243 },
-        ],
-      },
-    ],
     chart: {
-      type: "bar",
-      height: "420px",
+      height: "100%",
+      maxWidth: "100%",
+      type: "line",
       fontFamily: "Inter, sans-serif",
+      dropShadow: {
+        enabled: false,
+      },
       toolbar: {
         show: false,
       },
     },
-    plotOptions: {
-      bar: {
-        horizontal: false,
-        columnWidth: "70%",
-        borderRadiusApplication: "end",
-        borderRadius: 8,
-      },
-    },
     tooltip: {
-      shared: true,
-      intersect: false,
-      style: {
-        fontFamily: "Inter, sans-serif",
-      },
-    },
-    states: {
-      hover: {
-        filter: {
-          type: "darken",
-          value: 1,
-        },
-      },
-    },
-    stroke: {
-      show: true,
-      width: 0,
-      colors: ["transparent"],
-    },
-    grid: {
-      show: false,
-      strokeDashArray: 4,
-      padding: {
-        left: 2,
-        right: 2,
-        top: -14,
+      enabled: true,
+      x: {
+        show: false,
       },
     },
     dataLabels: {
       enabled: false,
     },
+    stroke: {
+      curve: "smooth",
+      width: 6,
+    },
+    grid: {
+      show: true,
+      strokeDashArray: 4,
+      padding: {
+        left: 2,
+        right: 2,
+        top: -26,
+      },
+    },
+    series: [],
     legend: {
       show: true,
     },
-    xaxis: {
-      floating: false,
-      labels: {
-        show: true,
-        style: {
-          fontFamily: "Inter, sans-serif",
-          cssClass: "text-xs font-normal fill-gray-400",
-        },
-      },
-      axisBorder: {
-        show: false,
-      },
-      axisTicks: {
-        show: false,
-      },
-    },
-    yaxis: {
-      show: false,
-    },
-    fill: {
-      opacity: 1,
-    },
   });
-
-  // useEffect(() => {
-  //   const chartElement = document.getElementById("column-chart");
-  //   if (chartElement && typeof ApexCharts !== "undefined") {
-  //     chartElement.innerHTML = "";
-  //     const chart = new ApexCharts(chartElement, options);
-  //     chart.render();
-  //   }
-  // }, []);
 
   interface formattedSeries {
     name: string;
@@ -159,7 +88,6 @@ export default function MetricsSection() {
         ?.data.push({ x: row[Headers.Date], y: row[Headers.Value] });
     });
     const dataSeries = Array.from(mapData.values()) as [];
-
     setOptions((prev) => ({ ...prev, series: dataSeries }));
   };
 
