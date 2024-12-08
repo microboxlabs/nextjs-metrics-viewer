16394e99f37ba57ac24bf4fcb1049b8c63d53078
Fabián Morag <fabianmorag0405@gmail.com>
2024-12-08 12:58:48 -0300

    redirect on login

--- nextjs-metrics-viewer/components/LoginForm.tsx
+++ nextjs-metrics-viewer/components/LoginForm.tsx
@@ -2,9 +2,11 @@
 import { signIn } from "next-auth/react";
 import { Button, Label, TextInput } from "flowbite-react";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
   const [error, setError] = useState(false);
+  const router = useRouter();
 
   const formAction = async (formData: FormData) => {
     const formCredentials = Object.fromEntries(formData);
@@ -12,8 +14,14 @@ export default function LoginForm() {
       ...formCredentials,
       redirect: false,
     });
-    if (response?.error) setError(true);
+
+    if (response?.error) {
+      setError(true);
+    } else {
+      router.push("/dashboard");
+    }
   };
+
   return (
     <div className="w-1/4 rounded-lg border border-gray-700 p-8">
       <form className="flex max-w-md flex-col gap-4" action={formAction}>
