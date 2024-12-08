e115f26288b875845761d620e00b1f2381bf7906
Fabián Morag <fabianmorag0405@gmail.com>
2024-12-08 13:29:12 -0300

    header over other elements

--- nextjs-metrics-viewer/components/Header.tsx
+++ nextjs-metrics-viewer/components/Header.tsx
@@ -12,7 +12,7 @@ import { signOut } from "next-auth/react";
 
 export default function Header() {
   return (
-    <Navbar className="fixed top-0 w-full border-b border-gray-700 bg-black px-4">
+    <Navbar className="fixed top-0 z-10 w-full border-b border-gray-700 bg-black px-4">
       <div className="m-auto flex h-16 w-full max-w-screen-xl items-center justify-between">
         <NavbarBrand href="#">
           <span className="self-center whitespace-nowrap text-xl font-semibold text-[#00BBCF]">
