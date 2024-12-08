cdc21d30c5bbea439c9dba2812bcf47aa321d13b
Fabián Morag <fabianmorag0405@gmail.com>
2024-12-08 13:39:19 -0300

    styled avatar button with session data

--- nextjs-metrics-viewer/components/Header.tsx
+++ nextjs-metrics-viewer/components/Header.tsx
@@ -8,9 +8,11 @@ import {
   NavbarBrand,
   NavbarToggle,
 } from "flowbite-react";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 export default function Header() {
+  const { data: session } = useSession();
+
   return (
     <Navbar className="fixed top-0 z-10 w-full border-b border-gray-700 bg-black px-4">
       <div className="m-auto flex h-16 w-full max-w-screen-xl items-center justify-between">
@@ -25,19 +27,22 @@ export default function Header() {
             inline
             label={
               <Avatar
-                alt="User settings"
-                img="https://flowbite.com/docs/images/people/profile-picture-1.jpg"
+                alt={session?.user.name ?? "User name"}
+                img={session?.user.img}
                 rounded
               />
             }
           >
             <DropdownHeader>
-              <span className="block text-sm">Bonnie Green</span>
-              <span className="block truncate text-sm font-medium">
-                name@flowbite.com
+              <span className="block text-sm font-medium">
+                {session?.user.name}
               </span>
+              <span className="block truncate text-sm ">MicroboxLabs</span>
             </DropdownHeader>
-            <DropdownItem onClick={() => signOut({ redirectTo: "/" })}>
+            <DropdownItem
+              className="text-red-500"
+              onClick={() => signOut({ redirectTo: "/" })}
+            >
               Sign out
             </DropdownItem>
           </Dropdown>
